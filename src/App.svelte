1873368ace8856a760a30715e53629237ace48b5
lou-ye <lourdesrosalesadan@gmail.com>
2022-08-21 17:04:29 -0300

    Main section structure and styles is added.

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Header from "./components/Header.svelte";
+	import Main from "./components/Main.svelte";
 </script>
 
 <style>
 </style>
 
 <Header />
+<Main />
---
+++ src/components/Main.svelte
@@ -0,0 +1,80 @@
+<style>
+    .Main {
+        width: 100%;
+        height: 500px;
+        font-size: 1.3rem;
+        position: relative;
+    }
+
+    .Main-section-description {
+        position: absolute;
+        width: 800px;
+        margin-top: 100px;
+        margin-left: 150px;
+    }
+
+    .Main-section-description h1 {
+        margin: 0;
+        font-size: 4rem;
+        font-weight: 700;
+    }
+
+    .Main-section-description h3 {
+        font-weight: 700;
+    }
+
+    .Main-section-description button {
+        width: 150px;
+        height: 50px;
+        padding: 10px 20px;
+        text-align: center;
+        display: inline-block;
+        font-size: 1.2rem;
+        font-weight: 1000;
+        color: darkcyan;
+        background-color: white;
+        border: 3px solid darkcyan;
+        border-radius: 50px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-family: 'Inter', sans-serif;
+        margin-top: 30px;
+    }
+
+    .Main-section-img {
+        position: absolute;
+        width: 400px;
+        height: 100%;
+        background-color: darkcyan;
+        margin-left: 1000px;
+    }
+
+</style>
+
+<div class="Main">
+    <section class="Main-section">
+        <div class="Main-section-description">
+            <h1>DAOVOTION</h1>
+            <h3>Apoya el financiamiento del OSS y gana dinero en el Metaverso.</h3>
+            <p>DAOVOTION es una plataforma de organizaciones decentralizadas (DAO), que gestiona el recaudo para proyectos de Software Libre (FOSS), mediante la venta de tierras ligadas al Metaverso Abierto: Las Meta Islas.</p>
+            <button>Saber mas</button>
+        </div>
+        <div class="Main-section-img">
+        </div>
+        <!-- <div>
+            <div>
+                <h1>DAOVOTION</h1>
+                <h3>Apoya el financiamiento del OSS y gana dinero en el Metaverso</h3>
+                <p>DAOVOTION es una plataforma de organizaciones decentralizadas (DAO), que gestiona el recaudo para proyectos de Software Libre (FOSS), mediante la venta de tierras ligadas al Metaverso Abierto: Las Meta Islas.</p>
+                <button>adqueri planete</button>
+            </div>
+            <div>
+                <h1>DAOVOTION</h1>
+                <h3>Apoya el financiamiento del OSS y gana dinero en el Metaverso</h3>
+                <p>DAOVOTION es una plataforma de organizaciones decentralizadas (DAO), que gestiona el recaudo para proyectos de Software Libre (FOSS), mediante la venta de tierras ligadas al Metaverso Abierto: Las Meta Islas.</p>
+                <button>crea proyecto</button>
+            </div>
+        </div> -->
+    </section>
+</div>
